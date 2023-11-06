2b9f3a6fe6974130a6576d1b479812ca58a1be47
Brcnerden <brcnerden@gmail.com>
2023-11-06 15:54:31 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Markets from "./component/Dropdown";
+
+function App() {
+  return (
+    <>
+      <Markets />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Dropdown.jsx
@@ -0,0 +1,69 @@
+import Dropdown from "react-bootstrap/Dropdown";
+
+function Markets() {
+  const marketsName = [
+    {
+      id: 1,
+      name: "Metro",
+    },
+    {
+      id: 2,
+      name: "Migros",
+    },
+    {
+      id: 3,
+      name: "Carrefoursa",
+    },
+  ];
+
+  const categoryNames = [
+    {
+      id: 1,
+      name: "Elektronik",
+    },
+
+    {
+      id: 2,
+      name: "Gıda",
+    },
+    {
+      id: 3,
+      name: "Deterjan",
+    },
+  ];
+
+  return (
+    <>
+      <div className="d-flex gap-2">
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Markets
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            {marketsName.map((market) => (
+              <Dropdown.Item href="#/action-3" key={market.id}>
+                {market.name}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Categories
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            {categoryNames.map((category) => (
+              <Dropdown.Item href="#/action-3" key={category.id}>
+                {category.name}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    </>
+  );
+}
+
+export default Markets;
